14b3df76ee43147febba8affc0931d87c81b2143
ayatsidielkhir <ayatsidielkhir@gmail.com>
2025-07-04 11:13:38 +0100

    trajet.model

--- backend/src/models/trajet.model.ts
+++ backend/src/models/trajet.model.ts
@@ -6,6 +6,7 @@ const trajetSchema = new mongoose.Schema({
   date: { type: Date, required: true },
   chauffeur: { type: mongoose.Schema.Types.ObjectId, ref: 'Chauffeur', required: true },
   vehicule: { type: mongoose.Schema.Types.ObjectId, ref: 'Vehicule', required: true },
+  remorque: { type: String, required: false },
   distanceKm: { type: Number, required: true },
   consommationL: { type: Number, required: true },
   consommationMAD: { type: Number, required: false },
